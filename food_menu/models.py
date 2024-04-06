d639c0b561347d46190ad08d575948333f90f613
jrms <email@fake.com>
2024-04-06 13:52:23 -0700

    Create food_menu Item model

--- food_menu/models.py
+++ food_menu/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Item(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.DecimalField(max_digits=6, decimal_places=2)
+    item_image = models.ImageField(upload_to='menu_images/', default='default.jpg')
+    
+    def __str__(self):
+        return self.item_name
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
