2b2c8b01fb9aab9f7ae4168c00487c082e1f9414
jrms <email@fake.com>
2024-04-06 14:18:55 -0700

    Import food menu to main project urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from food_menu import views as food_menu_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+
 ]
 
 if settings.DEBUG:
