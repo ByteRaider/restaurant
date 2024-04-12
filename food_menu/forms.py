40512ad78a47e9f4000d890fa346b230635e9b50
jrms <email@fake.com>
2024-04-11 20:53:10 -0700

    Edit detail page

--- food_menu/forms.py
+++ food_menu/forms.py
@@ -5,3 +5,10 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['item_name','item_desc','item_price','item_image']
+    
+    widgets = {
+        'item_name': forms.TextInput(attrs={'class': 'form-control'}),
+        'item_desc': forms.TextInput(attrs={'class': 'form-control'}),
+        'item_price': forms.NumberInput(attrs={'class': 'form-control'}),
+        'item_image': forms.FileInput(attrs={'class': 'form-control'}),
+    }
